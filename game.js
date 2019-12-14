37a97e6e1164396bfb58562e2626d7dad68af51e
vagrant <vagrant@vagrant.vm>
2019-12-14 00:25:15 +0000

    Created js file

---
+++ game.js
@@ -0,0 +1,17 @@
+const letterChoice = [
+    " D, R, I, P ", 
+    " A, P , K, R",
+    " D, T, E, N ",
+    " A, E, B, T ",
+];
+
+const letterList = [["DRIP", "RID", "DIP", "RIP"],
+              ["PARK", "ARK", "PAR", "RAP"],
+              ["DENT", "TEND", "END", "DEN", "NET","TEND"],
+              ["BAT", "BET", "EAT", "BETA", 'TEA', "TAB", 
+              "ATE", "BEAT"]];
+
+
+const createDivsForLetterChoice = (word) => {
+    for
+}
